d8e87f0592fac79603cf1e0d82acefcfc0cffaaa
anithachowdary23 <108563339+anithachowdary23@users.noreply.github.com>
2024-07-06 10:41:40 +0530

    pro.py and eligible.py

---
+++ eligible.py
@@ -0,0 +1,7 @@
+age=int(input("enter the age of the person"))
+if age>18:
+    print("eligible for voting")
+elif age<18:
+    print("not eligible for voting")
+else:
+    print("bye")
---
+++ pro.py
@@ -0,0 +1,9 @@
+n=5
+for i in range (5):
+    if i>1:
+        if(n%i==0):
+            print("the number is prime")
+    else:
+        print("not prime")
+else:
+    print("bye")
